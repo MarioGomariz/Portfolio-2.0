9aad3fb1929060e6a30a7cca7dcc8596611ae954
MarioGomariz <m.gomariz01@gmail.com>
2025-08-19 03:16:47 -0300

    fix build

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -153,7 +153,7 @@ export default function ProjectsPage() {
       {/* Resultados */}
       {filteredProjects.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-          {filteredProjects.map((project, index) => (
+          {filteredProjects.map((project) => (
             <div key={project.slug}>
               <ProjectCard project={project} />
             </div>
