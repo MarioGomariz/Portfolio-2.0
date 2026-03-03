0035572a20fe6f9b79353a38e1a45e549527e912
Mario <m.gomariz01@gmail.com>
2026-03-03 01:18:17 -0300

    change view proy

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
-        {projects.slice(0, 3).map((project, index) => (
+        {projects.slice(0, 6).map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
       </div>
